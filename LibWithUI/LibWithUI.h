afa134d3fa73bb5f9b93686e8bb5ba80c25ab197
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-11-16 03:41:22 +0100

    first commit

---
+++ LibWithUI/LibWithUI.h
@@ -0,0 +1,19 @@
+//
+//  LibWithUI.h
+//  LibWithUI
+//
+//  Created by Luiz Rodrigo Martins Barbosa on 16.11.19.
+//  Copyright © 2019 Luiz Rodrigo Martins Barbosa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibWithUI.
+FOUNDATION_EXPORT double LibWithUIVersionNumber;
+
+//! Project version string for LibWithUI.
+FOUNDATION_EXPORT const unsigned char LibWithUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibWithUI/PublicHeader.h>
+
+
---
+++ LibWithUI/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  LibWithUI
+//
+//  Created by Luiz Rodrigo Martins Barbosa on 16.11.19.
+//  Copyright © 2019 Luiz Rodrigo Martins Barbosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
